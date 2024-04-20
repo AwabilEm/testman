1b3c5a83d988e7e9e683d34001995495bc29849e
Kodmindset <kodemindset@gmail.com>
2024-04-20 09:19:37 +0000

    first commit8

--- tests/newonboarding.spec.ts
+++ tests/newonboarding.spec.ts
@@ -38,44 +38,6 @@ test('Onboarding', async ({ page }) => {
  await FirstAppointment(page);
 
 });
-test('AdminDasboard', async ({ page }) => {
-  await page.goto('https://admin.manduu.app/account/login');
-await page.getByPlaceholder('Email Address or Phone Number').click();
-await page.getByPlaceholder('Email Address or Phone Number').fill(process.env.NEWADUSERNAME!);
-await page.getByPlaceholder('Password').fill(process.env.NEWADPASSWORD!);
-await page.getByRole('button', { name: 'Login' }).click();
-await page.getByRole('link', { name: ' Clients' }).click();
-await page.getByRole('link', { name: ' Client Session' }).click();
-await page.fill('[formcontrolname="selectedDate"]', '17 September, 2024');
-await page.getByText('Select studio').click();
-await page.getByText('Edmond Oklahoma').click();
-// await page.locator('a').filter({ hasText: (fName) && (lName) }).click();
-//const fullName = `${firstName} ${lastName}`;
-await page.locator('a').filter({ hasText: (fName) && (lName) }).click();
-
-await page.getByLabel('Status').selectOption('Executed');
-await page.getByLabel('Personal coach / Trainer').selectOption('307');
-await page.getByLabel('Client memo').fill('testing');
-await page.getByRole('button', { name: 'Save' }).click();
-await page.waitForTimeout(2000);
-
-
-});
-test('LoginToCompleteOnboard', async ({ page }) => {
-    
-  await page.goto('https://newpwa.manduu.app/account/login');
-  await page.getByPlaceholder('Username Or Email *').fill(email);
-  await page.getByPlaceholder('Password *').fill(password);
-  await page.getByRole('button', { name: 'Log In' }).click();
- 
-  await addCard(page)
-  await signContract(page)
-  
-  
-     });
-
-
-     
 
 async function fillPersonalInformation(page: any) {
    
